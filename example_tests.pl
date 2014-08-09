7b03fe271ad0b5fdebd418d4d75adc710d513478
cogwirrel <cogwirrel@gmail.com>
2014-08-09 22:24:11 +0100

    Spaces -> tabs

--- example_tests.pl
+++ example_tests.pl
@@ -8,15 +8,15 @@ test :-
 
 %%% Environments %%%
 henry_father_of_jack_and_harry([
-    father(henry, jack),
-    father(henry, harry)
+	father(henry, jack),
+	father(henry, harry)
 ]).
 
 %%% Tests %%%
 siblingTest :-
-    with(henry_father_of_jack_and_harry,[
-        sibling(jack, harry)
-    ]).
+	with(henry_father_of_jack_and_harry,[
+		sibling(jack, harry)
+	]).
 
 notSiblingTest :-
 	with(henry_father_of_jack_and_harry,[
